a3a4c1d0503a3080ec54911ad484bf1532b8d98d
Timothy Montgomery <tim.montgomery95@gmail.com>
2022-10-04 16:48:43 -0400

    added patch request to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,28 @@
 class ReviewsController < ApplicationController
     get "/reviews" do
-        reviews = Review.all
-        reviews.to_json
+        review = Review.all
+        review.to_json
     end
 
     get "reviews/:id" do
-        reviews = Review.find(params[:id])
-        reviews.to_json
+        review = Review.find(params[:id])
+        review.to_json
     end
 
     post "/reviews" do
-       reviews = Review.create(name:params[:name])
+       review = Review.create(munchie_rating:params[:munchie_rating], review_text:params[:review_text], user_id:params[:user_id], restaurant_id:params[:restaurant_id])
        status 201   #201 = created
-       reviews.to_json
+       review.to_json
+    end
+
+    patch "reviews/:id" do
+        review = Review.find(params[:id])
+        review.update(munchie_rating:params[:munchie_rating], review_text:params[:review_text])
+        status 202  #202 = accepted
     end
 
     delete "/reviews/:id" do
-        reviews = Review.find(params[:id])
+        review = Review.find(params[:id])
         Review.destroy
         status 204  #204 = no content
     end
