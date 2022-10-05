091cc6d5833a5b3cb8ef86f9a4c23d4ecf8c252c
Timothy Montgomery <tim.montgomery95@gmail.com>
2022-10-05 11:11:13 -0400

    added top review method to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,15 +2,15 @@ class Restaurant < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
 
-    def top_review
-        self.reviews.order(munchie_rating: :desc).first
-    end
-
     def all_reviews
         self.reviews.all
     end
 
     def self.average_rating
         self.reviews.all.average(:munchie_rating).to_i
+    end
+
+    def top_review
+        self.reviews.order(munchie_rating: :desc).first
     end
 end
